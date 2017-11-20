6e763287bdf180d792968b3886b1540efadf14ee
Sven Vinkemeier <svenvinkemeier@live.de>
2017-11-20 09:57:01 +0100

    Added NuGet/NPM packaging scripts (only created with option --MakeSdk)

---
+++ Template/NPMPack.ps1
@@ -0,0 +1,28 @@
+Switch ("$env:Build_SourceBranchName")
+{
+    "master" { $env:tag = "master"  }
+    "develop" { $env:tag = "develop" }
+    default { exit }
+}
+
+cd *.Typescript/package
+$regUrl = $env:NPMFeed.Replace("http:","").Replace("https:","")
+Set-Content -Value "$($regUrl):_authToken=$env:MyGetKey" -Path ./.npmrc
+npm install
+
+Switch ("$env:Build_SourceBranchName") 
+{
+    "develop"
+    {
+        npm --% publish  --registry=%MyGetFeed% --tag %tag%
+    }
+    "master"
+    {
+        $json = Get-Content -Path package.json | ConvertFrom-Json
+        $env:version = $json.version
+        $env:name = $json.name
+        npm --% dist-tag add %name%@%version% %tag%
+    }
+}
+
+$LASTEXITCODE = 0
